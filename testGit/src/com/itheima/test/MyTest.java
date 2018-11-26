e53d3df7ca8e13845ee5248b5cdf7bce54dbec89
xjh <1183044435@qq.com>
2018-11-26 20:16:48 +0800

    java project12

--- testGit/src/com/itheima/test/MyTest.java
+++ testGit/src/com/itheima/test/MyTest.java
@@ -5,5 +5,7 @@ public class MyTest {
     public static void main(String[] args) {
 
         System.out.println("hello world!");
+        System.out.println("hello world!");
+        System.out.println("hello world!");
     }
 }
